68a93e69203c6b74628290b6942f2d6a6cd3df08
nikita_siniukov <ns.engineering.tech@gmail.com>
2021-06-15 07:44:26 +0300

    FIXES

--- resources/js/core/routes.js
+++ resources/js/core/routes.js
@@ -1,17 +1,11 @@
 import NotFound from "./components/NotFound";
 import Create from "../modules/Chart/components/CreateChart";
-import CreateCharts from "../modules/Chart/components/CreateCharts";
 
 export const routes = [
     {
         path: '/',
         name: 'CreateChart',
         component: Create,
-    },
-    {
-        path: '/charts',
-        name: 'CreateCharts',
-        component: CreateCharts,
     },
     {
         path: '*',
