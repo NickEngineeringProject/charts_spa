35a8dc6dc815a4ccdebf4f0568ced600ab2f15aa
nikita_siniukov <ns.engineering.tech@gmail.com>
2021-06-26 10:57:16 +0300

    FIXES

--- app/Services/Chart/Series.php
+++ app/Services/Chart/Series.php
@@ -8,41 +8,34 @@ use Illuminate\Support\Facades\Response;
 
 class Series
 {
+    public function stringHandler(string $value): array
+    {
+        return explode(",", str_replace(" ", "", $value));
+    }
+
+    public function advancedHandler(string $value, string $name): array
+    {
+        $value = $this->stringHandler($value);
+        $name = $this->stringHandler($name);
+
+        $data = [];
+        for ($i = 0; $i<count($value); $i++) {
+            $data[] = (object)["value" => (int)$value[$i], "name" => $name[$i]];
+        }
+
+        return $data;
+    }
+
     public function dataset(string $type, string $value, ?string $name = null)
     {
         try {
-            $value = explode(",", str_replace(" ", "", $value));
-            $name = explode(",", str_replace(" ", "", $name));
-
-            switch ($type) {
-                case "line":
-                    return ["data" => $value, "type" => $type, "smooth" => false];
-                    break;
-                case "pie":
-                    $data = [];
-                    for ($i = 0; $i<count($value); $i++) {
-                        $data[] = (object)["value" => (int)$value[$i], "name" => $name[$i]];
-                    }
-                    return ["radius" => "50%", "data" => $data];
-                    break;
-                case "scatter":
-                    //TODO: протестировать ввод 1,3 параметров(ринимает 2)
-                    //TODO: продумать изменение свойства symbolSize а также подсказки для пользователя типа что такое стандартная диаграмма
-                    $data = [];
-                    for ($i = 0; $i<count($value); $i++) {
-                        $data[] = [$value[$i], $value[++$i]];
-                    }
-                    return ["symbolSize" => 20, "data" => $data, "type" => $type];
-                case "k":
-                    //TODO: протестировать ввод 3,5 параметров(принимает 4)
-                    $data = [];
-                    for ($i = 0; $i<count($value); $i++) {
-                        $data[] = [$value[$i], $value[++$i], $value[++$i], $value[++$i]];
-                    }
-                    return ["type" => $type, "data" => $data];
-                default:
-                    return Response::json(["status" => "error", "message" => "Данного типа диаграммы не существует!"], 500);
-            }
+            return match ($type) {
+                "line" => ["data" => $this->stringHandler($value), "type" => $this->stringHandler($type), "smooth" => false],
+                "pie" => ["radius" => "50%", "data" => $this->advancedHandler($value, $name)],
+                "scatter" => ["symbolSize" => 20, "data" => $this->advancedHandler($value, $name), "type" => $type],
+                "k" => ["type" => $type, "data" => $this->advancedHandler($value, $name)],
+                default => Response::json(["status" => "error", "message" => "Данного типа диаграммы не существует!"], 500),
+            };
         } catch (\Exception) {
             return Response::json(["status" => "error", "message" => "Ошибка в обработке данных объекта Series"], 500);
         }
